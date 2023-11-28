c5d43a94a7209abecc776a381826625b4d91088b
IkerFeda <iferndac@myuax.com>
2023-11-28 11:55:07 +0100

    .

--- To_String.cpp
+++ To_String.cpp
@@ -1,3 +1,21 @@
-//
-// Created by ikerf on 28/11/2023.
-//
+#include <string>
+#include <iostream>
+
+class Variant {
+public:
+    Variant(int value) : m_value(value) {}
+
+    std::string to_string() const {
+        return std::to_string(m_value);
+    }
+
+private:
+    int m_value;
+};
+
+int main() {
+    Variant v(42);
+    std::string str = v.to_string();
+    std::cout << str << std::endl;
+    return 0;
+}
